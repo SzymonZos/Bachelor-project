342e7405a4bb76fcf72169b09e8af584de8e8d66
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2020-01-19 20:08:04 +0100

    Fixes in script required

--- PC-STM-communication/playground.py
+++ PC-STM-communication/playground.py
@@ -1,29 +1,6 @@
-import numpy as np
 import re
 import os
 from contextlib import ExitStack
-
-
-
-def power_iteration(A, num_simulations: int):
-    # Ideally choose a random vector
-    # To decrease the chance that our vector
-    # Is orthogonal to the eigenvector
-    b_k = np.random.rand(A.shape[1])
-    print(b_k)
-
-    for _ in range(num_simulations):
-        # calculate the matrix-by-vector product Ab
-        b_k1 = np.dot(A, b_k)
-
-        # calculate the norm
-        b_k1_norm = np.linalg.norm(b_k1)
-        # print('my norm: ' + str(np.sqrt(np.dot(b_k1, b_k1))))
-
-        # re normalize the vector
-        b_k = b_k1 / b_k1_norm
-
-    return np.dot(np.dot(A, b_k), b_k) / np.dot(b_k, b_k)
 
 
 def main():
@@ -31,7 +8,7 @@ def main():
     filenames = os.listdir(filepath)
 
     with ExitStack() as stack:
-        files = [stack.enter_context(open(filepath + filename, 'r', encoding='UTF-8')) for filename in filenames]
+        files = [stack.enter_context(open(filepath + filename, 'w+', encoding='UTF-8')) for filename in filenames]
         texfiles_copy = {filename: file.read() for file, filename in zip(files, filenames)}
         for filename, file_content in texfiles_copy.items():
             listings = []
@@ -45,7 +22,8 @@ def main():
                 else:
                     texfiles_copy[filename] = texfiles_copy[filename][:match.start()] + '~' +\
                                               texfiles_copy[filename][match.start()+1:]
-            print(texfiles_copy[filename])
+        for file, filename in zip(files, texfiles_copy):
+            file.write(texfiles_copy[filename])
 
 
 if __name__ == '__main__':
