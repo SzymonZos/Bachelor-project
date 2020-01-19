4dd45efbc472375122f3703ec0d18a50db098a3b
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2020-01-19 19:54:13 +0100

    Remove orphans

--- PC-STM-communication/playground.py
+++ PC-STM-communication/playground.py
@@ -1,4 +1,8 @@
 import numpy as np
+import re
+import os
+from contextlib import ExitStack
+
 
 
 def power_iteration(A, num_simulations: int):
@@ -23,7 +27,25 @@ def power_iteration(A, num_simulations: int):
 
 
 def main():
-    print(power_iteration(np.array([[2, -12], [1, -5]]), 10))
+    filepath = r'../thesis/chapters/'
+    filenames = os.listdir(filepath)
+
+    with ExitStack() as stack:
+        files = [stack.enter_context(open(filepath + filename, 'r', encoding='UTF-8')) for filename in filenames]
+        texfiles_copy = {filename: file.read() for file, filename in zip(files, filenames)}
+        for filename, file_content in texfiles_copy.items():
+            listings = []
+            for match in re.finditer(r'\\begin{listing\}.+?\\end{listing\}', file_content, re.S | re.M):
+                listings.append((match.start(), match.end()))
+            print(listings)
+            for match in re.finditer(r'((?<= \w) )|((?<= \w\w) )', file_content):
+                for listing in listings:
+                    if listing[0] < match.start() < listing[1]:
+                        break
+                else:
+                    texfiles_copy[filename] = texfiles_copy[filename][:match.start()] + '~' +\
+                                              texfiles_copy[filename][match.start()+1:]
+            print(texfiles_copy[filename])
 
 
 if __name__ == '__main__':
